fbb453d24c005a71b4c8884001f45602c28f646e
kihno <stistyle13@gmail.com>
2023-02-27 17:01:45 -0800

    Find shortest path to destination

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -1,24 +1,12 @@
 require_relative 'square'
 
 class Gameboard
-  def initialize(root = nil)
+  def initialize
     @grid = make_board
-    # @root = build_graph(@grid)
   end
-  
-  # def build_graph(array)
-  #   array.map.with_index do |row, rank_index|
-  #     rank = rank_index
-  #     row.map.with_index do |node, file_index|
-  #       file = file_index
-  #       node = Square.new([rank, file])
-  #       # node.moves = add_edges(node, [rank, file])
-  #     end
-  #   end.reverse
-  # end
 
   def make_board
-    grid = Array.new(8, Array.new(8))
+    grid = Array.new(8) { Array.new(8) }
     grid.map.with_index do |row, rank_index|
       rank = rank_index
       row.map.with_index do |node, file_index|
@@ -28,82 +16,89 @@ class Gameboard
     end
   end
 
+  def start
+    print_board
+    puts "Enter your starting location:"
+    start = get_location(gets.chomp.upcase)
+
+    puts "Enter your destination"
+    finish = get_location(gets.chomp.upcase)
+  
+    knight_moves(start, finish)
+  end
+
+  def pathfinder(finish, parents)
+    path = []
+    node = finish
+
+    until node.nil?
+      path << node
+      node = parents[node]
+    end
+
+    print_board(path.reverse)
+  end
+
+  def get_location(name)
+    @grid.each do |row|
+      row.each do |node|
+        return node.location if node.name == name
+      end
+    end
+  end
+
   def knight_moves(start, finish)
     queue = []
     visited = []
+    parents = { start => nil }
 
     queue.push(start)
 
     until queue.empty?
       current = queue[0]
 
-      return visited << @grid[current[0]][current[1]].location if current == finish
+      return pathfinder(finish, parents) if current == finish
 
       if !visited.include?(current)
         visited << current
-        @grid[current[0]][current[1]].moves.each { |move| queue << move }
-      end 
+        @grid[current[0]][current[1]].moves.each do |move|
+          unless parents.key?(move)
+            parents[move] = current
+            queue << move
+          end
+        end
+      end
       queue.shift
     end
-    visited
   end
 
-  # def knight_moves(start, finish)
-  #   visited = level_order(start, finish)
-
-  #   path = [finish]
-  #   current = finish
-  #   until current == start
-  #     @grid[current[0]][current[1]].moves.each do |move|
-  #       if visited.include?(move) && @grid[move[0]][move[1]] == @grid[current[0]][current[1]] - 1
-  #         path.unshift(move)
-  #         current = move
-  #         break
-  #       end
-  #     end
-  #   end
-  #   path
-  # end
-
-
-  # def add_edges(node)
-  #   moves = [[1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2]]
-
-  #   moves.each do |move|
-  #     row = node.location[0] + move[0]
-  #     col = node.location[1] + move[1]
-    
-  #     if row.between?(0, 7) && col.between?(0, 7)
-  #       node.moves << @grid[row][col]
-  #     end
-  #   end
-  # end
-
-  # def add_edges(node, location)
-  #   moves = [[1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2]]
-  #   valid_moves = []
-
-
-  #   moves.each do |move|
-  #     new_space = [location[0] + move[0], location[1] + move[1]]
-    
-
-  #     if new_space[0].positive? && new_space[0] < 8 && new_space[1].positive? && new_space[1] < 8
-  #       valid_moves << @root[new_space[0]][new_space[1]]
-  #     end
-  #   end
-  #   valid_moves
-  # end
-
-  def print_board
-    index = 0
+  def print_board(path)
     board = []
     @grid.each do |row|
       rank = []
-      row.each { |square| rank.push(square.location) }
+      row.each { |square| rank.push(square) }
       board.push(rank)
     end
 
-    board.each { |row| p row }
+    board.each do |row|
+      name_row = []
+
+      row.each do |square|
+        if square.location == path[0]
+          name_row << 'ST'
+        elsif square.location == path[-1]
+          name_row << 'EN'
+        elsif path.include?(square.location)
+          name_row << "0#{path.index(square.location)}"
+        else
+          name_row << square.name
+        end
+      end
+
+      p name_row
+    end
+
+    puts "You made it in #{path.length - 1} moves. Here is your path:"
+    path.each { |move| puts board[move[0]][move[1]].name }
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,5 @@
 require_relative 'gameboard'
-require_relative 'knight'
 
 gameboard = Gameboard.new
 
-
-p gameboard.knight_moves([3, 3], [4, 3])
-
+gameboard.knight_moves([3, 3], [4, 3])
